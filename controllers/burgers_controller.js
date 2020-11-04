e63a68065f0cad2a079c88b1ae7eeb5e2c58ae28
John Melendez <john@Johns-MBP.domain>
2020-11-03 21:25:23 -0700

    added to controllers

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,19 @@
+// import Express & burger.js
+var express = require("express");
+
+// create router object
+var router = express.Router();
+
+// import burger.js MODEL for database functions
+let burger = require("../models/burger.js");
+
+// Main Root directory.
+router.get("/", function (req, res) {
+    burger.all( (db_data) => {
+        let burgerHBSObj = {
+            burgers: db_data
+        }
+        console.log("burgerHBSObj: ", burgerHBSObj);
+        res.render("index", burgerHBSObj);
+    });
+});
